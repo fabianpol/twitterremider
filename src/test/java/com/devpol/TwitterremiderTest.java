a548a6ed218a602c79a2b0132e730c5be58a33b1
Fabian <fabianpol>
2021-04-17 17:37:43 +0000

    Initial commit

---
+++ src/test/java/com/devpol/TwitterremiderTest.java
@@ -0,0 +1,21 @@
+package com.devpol;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+class TwitterremiderTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
